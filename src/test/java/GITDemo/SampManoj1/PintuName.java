7a1997221b98c055b6ed15e07e545592cc5d55b6
manoj <lavanya.c102@gmail.com>
2025-06-26 12:10:01 +0530

    this is the commit from manoj in asian time zone

---
+++ src/test/java/GITDemo/SampManoj1/PintuName.java
@@ -0,0 +1,12 @@
+package GITDemo.SampManoj1;
+
+public class PintuName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi am manoj kumar, 30yrs old");
+
+	}
+
+}
