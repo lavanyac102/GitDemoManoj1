7675fe2252248b1dcce2be99357b6e7fa02c4013
manoj <lavanya.c102@gmail.com>
2025-06-26 13:38:49 +0530

    update from asian time line

--- src/test/java/GITDemo/SampManoj1/PintuName.java
+++ src/test/java/GITDemo/SampManoj1/PintuName.java
@@ -9,6 +9,7 @@ public class PintuName {
 		System.out.println("Hi bought a BMW in the year 2022");
 		System.out.println("i bought skoda in the year 2025");
 		System.out.println("I met lavanya for the first time on may 4th 2025 in polarbear");
+		System.out.println("i broke up with lavanya on 20th june 2025");
 
 	}
 
